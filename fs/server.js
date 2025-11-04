256027c3f47b1300ee4324d987acdf5ff6a8422e
Basant Kumar Mahato <basantkumar72580@gmail.com>
2025-11-04 15:51:18 +0530

    fs

---
+++ fs/server.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+
+// fs.writeFileSync('./test.txt', "Demo text")
+fs.writeFile("./test.txt", "Demo text", (err)=>{
+
+})
+
+fs.readFile("./test.txt", "utf-8", (err, result)=>{
+
+    if(err){
+        console.log(err)
+    } else{
+        console.log(result)
+    }
+    
+})
+
+
+fs.appendFileSync("./test.txt", "hehehe" )
+
+const textData = fs.readFileSync("./test.txt", "utf-8")
+
+console.log(textData)
