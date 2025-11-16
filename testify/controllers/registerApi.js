1c6204c791c369302e67c100ca26f0a777de7e1a
Basant Kumar Mahato <basantkumar72580@gmail.com>
2025-11-16 22:24:50 +0530

    password hash not working

--- testify/controllers/registerApi.js
+++ testify/controllers/registerApi.js
@@ -2,30 +2,23 @@ const mongoose = require("mongoose");
 const User = require("../models/user");
 
 const registerApi = async (req, res) => {
+  try {
+    const userData = req.body;
+    const existingUser = await User.findOne({ email: userData.email });
+    console.log("User data received:", userData);
 
-    try {
-        const userData = req.body;
-        console.log("User data received:", userData);
-
-
-        if( User.findOne({email: userData.email})){
-
-            res.send({message: "User already exists", data: userData})
-
-        }else
-            {
-        const user = new User(userData);
-        await user.save();
-        res.send({ message: "User registered successfully", data: userData });
-        console.log({ message: 'User registered successfully', data: savedUser });
-}
-
-        
-    }catch(err){
-        res.status(400).send({ error: error.message });
-        console.log({ error: error.message });
+    if (existingUser) {
+      res.send({ message: "User already exists", data: userData });
+    } else {
+      const user = new User(userData);
+      const savedUser = await user.save();
+      res.send({ message: "User registered successfully", data: savedUser });
+      console.log({ message: "User registered successfully", data: savedUser });
     }
-
-}
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+    console.log({ error: err.message });
+  }
+};
 
 module.exports = registerApi;
